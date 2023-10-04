c8f3c52b371ff268ea42266cd23b890287802296
J-Mardo01 <mar20080@byui.edu>
2023-10-04 16:10:21 -0600

    Update team.py

--- week04/team/team.py
+++ week04/team/team.py
@@ -22,26 +22,31 @@ import json
 # Include cse 251 common Python files
 from cse251 import *
 
-RETRIEVE_THREADS = 4        # Number of retrieve_threads
+RETRIEVE_THREADS = 1        # Number of retrieve_threads
 NO_MORE_VALUES = 'No more'  # Special value to indicate no more items in the queue
 
-def retrieve_thread():  # TODO add arguments
+def retrieve_thread(q:queue.Queue):  # TODO add arguments
     """ Process values from the data_queue """
 
     while True:
         # TODO check to see if anything is in the queue
-
-        # TODO process the value retrieved from the queue
+        if q.size() > 0:
+            # TODO process the value retrieved from the queue
+            q.get()
+            pass
+        else:
+            pass
 
         # TODO make Internet call to get characters name and log it
         pass
 
 
 
-def file_reader(): # TODO add arguments
+def file_reader(q:queue.Queue, log): # TODO add arguments
     """ This thread reading the data file and places the values in the data_queue """
-
     # TODO Open the data file "urls.txt" and place items into a queue
+    with open("urls.txt", "r") as data_file:
+        q.put(data_file)
 
     log.write('finished reading file')
 
@@ -55,10 +60,13 @@ def main():
     log = Log(show_terminal=True)
 
     # TODO create queue
+    q = queue.Queue()
     # TODO create semaphore (if needed)
 
     # TODO create the threads. 1 filereader() and RETRIEVE_THREADS retrieve_thread()s
     # Pass any arguments to these thread need to do their job
+    for x in range(RETRIEVE_THREADS):
+        pass
 
     log.start_timer()
 
