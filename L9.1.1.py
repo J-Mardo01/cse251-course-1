786d944d32a74f40c5926431bbbf15d636465895
J-Mardo01 <mar20080@byui.edu>
2023-11-08 15:29:27 -0700

    Class lesson code.

--- L9.1.1.py
+++ L9.1.1.py
@@ -42,8 +42,8 @@ def main():
     with open("jon.json", "w") as file:
         file.write(str(data))
     
-    with open("jon.bin", "rb") as file:
-        new_bytes = file.read()
+    #with open("jon.bin", "rb") as file:
+    #    new_bytes = file.read()
 
     jon = Person("Jon", 24, 1.77)
     jon.add_child("Jon III", 7, 1)
@@ -51,8 +51,16 @@ def main():
     jon.favorite_child_award(0)
 
     jon_bytes = pickle.dumps(jon)
+
     with open("jon.obj", "wb") as file:
         file.write(jon_bytes)
+
+    with open("jon.obj", "rb") as file:
+        jon_bytes_read= file.read()
+
+    jon_bytes_deserialized = pickle.loads(jon_bytes_read)
+
+    print(jon_bytes_deserialized.children[jon_bytes_deserialized.favorite_child].name)
 
 
 if __name__ == "__main__":
